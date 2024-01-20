81b3ae4192e34397e2b2c0f6631710b40079524d
Kamran Kiani <kiani.km@gmail.com>
2024-01-20 10:25:31 +0330

    Change icon plus and minus with java script

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const accordion = document.getElementsByClassName('container');
 
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function() {
+    // Remove active class when click on another container
+    for (let j = 0; j < accordion.length; j++) {
+      if (accordion[j] !== this) {
+        accordion[j].classList.remove('active');
+        // Change src to plus 
+        accordion[j].querySelector('.plus-minus-icon').src = 'assets/images/icon-plus.svg';
+      }
+    }
+
+    this.classList.toggle('active');
+    
+    const icon = this.querySelector('.plus-minus-icon');
+    icon.src = (icon.src.endsWith('icon-minus.svg')) ? 'assets/images/icon-plus.svg' : 'assets/images/icon-minus.svg';
+  });
 }
